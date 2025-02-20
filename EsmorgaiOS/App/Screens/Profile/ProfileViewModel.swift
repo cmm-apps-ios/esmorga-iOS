9aa7718d9c415d509353aff35094fd19d1b4bbdc
yagoaresarmesto <59642410+yagoaresarmesto@users.noreply.github.com>
2025-02-20 11:24:57 +0100

    Adding profileViewModelMapper in ProfileViewModel

--- EsmorgaiOS/App/Screens/Profile/ProfileViewModel.swift
+++ EsmorgaiOS/App/Screens/Profile/ProfileViewModel.swift
@@ -17,6 +17,7 @@ class ProfileViewModel: BaseViewModel<ProfileViewStates> {
     
     //Model LocalUser
     private let getLocalUserUseCase: GetLocalUserUseCaseAlias
+    private let mapper: ProfileViewModelMapper
     //Model userData
     @Published var user: UserModels.User?
     //Model loggedOut
@@ -24,8 +25,12 @@ class ProfileViewModel: BaseViewModel<ProfileViewStates> {
                                                               title: LocalizationKeys.Common.unauthenticatedTitle.localize(),
                                                               buttonText: LocalizationKeys.Buttons.login.localize())
     
-    init(coordinator: (any CoordinatorProtocol)? = nil, getLocalUserUseCase: GetLocalUserUseCaseAlias = GetLocalUserUseCase()) {
+    //Init
+    init(coordinator: (any CoordinatorProtocol)? = nil,
+         getLocalUserUseCase: GetLocalUserUseCaseAlias = GetLocalUserUseCase(),
+         mapper: ProfileViewModelMapper = ProfileViewModelMapper()) {
         self.getLocalUserUseCase = getLocalUserUseCase
+        self.mapper = mapper
         super.init(coordinator: coordinator)
     }
     
@@ -44,6 +49,34 @@ class ProfileViewModel: BaseViewModel<ProfileViewStates> {
     
     func loginButtonTapped() {
         coordinator?.push(destination: .login)
+    }
+    
+    func optionTapped(type: ProfileModels.OptionsItemType){
+        switch type {
+        case .changePassword:
+            //To add ->   coordinator?.push(destination: .changePassword)
+        case .closeSession:
+            //logoutUserUseCase.execute (to define)
+        }
+    }
+    //Profile Mapper
+    class ProfileViewModelMapper {
+        func map(user: UserModels.User) -> ProfileModels.LoggedModel {
+            
+            let userItems = [ProfileModels.UserDataItem(title: "Nombre", value: user.name + user.lastName, type: .name),
+                             ProfileModels.UserDataItem(title: "Email", value: user.email, type: .email)]
+            
+            
+            let optionsItems = [ProfileModels.OptionItem(title: "Cambiar Contraseña",
+                                                         image: "arrow.right",
+                                                         type: .changePassword),
+                                ProfileModels.OptionItem(title: "Cerrar sesión",
+                                                         image: "arrow.right",
+                                                         type: .closeSession)]
+
+            return ProfileModels.LoggedModel(userSection: ProfileModels.UserData(items: userItems), optionsSection: ProfileModels.Options(title: "Opciones", items: optionsItems))
+                
+        }
     }
     
 }

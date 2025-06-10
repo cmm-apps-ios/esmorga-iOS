e47101a3b5de603287a9943ef414902ab9391a58
yagoaresarmesto <59642410+yagoaresarmesto@users.noreply.github.com>
2025-06-10 12:17:47 +0200

    Modified DeepLinkManager to handle more types of code from links

--- EsmorgaiOS/App/Screens/ResetPassword/ResetPasswordViewModel.swift
+++ EsmorgaiOS/App/Screens/ResetPassword/ResetPasswordViewModel.swift
@@ -15,16 +15,18 @@ class ResetPasswordViewModel: BaseViewModel<ActivateAccountViewStates> {
 
     @Published var textFields = [ResetPasswordModels.TextFieldModels]()
 
-    @Published var primaryButton = RecoverPasswordModels.Button(title:"dfsfd",
+    @Published var primaryButton = RecoverPasswordModels.Button(title:"Cambiar constraseña",
                                                                 isLoading: false)
 
     init(coordinator: (any CoordinatorProtocol)?, code: String) {
+
+        print("Código recibido al viewModel: \(code)")
 
         super.init(coordinator: coordinator)
 
         textFields =  [ResetPasswordModels.TextFieldModels(type: .pass,
                                                            text: "",
-                                                           title: "Nueva Constraseña",
+                                                           title: "Nueva Contraseña",
                                                            placeholder: "Introduce la nueva contraseña",
                                                            isProtected: true),
                        ResetPasswordModels.TextFieldModels(type: .confirmPass,

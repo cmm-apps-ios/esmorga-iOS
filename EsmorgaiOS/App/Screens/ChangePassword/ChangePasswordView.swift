b68e28bca4e1544b119543ba86dba24f0987a7bc
Marcelo M. <marcelo.moran@accenture.com>
2026-08-27 14:20:30 +0200

    feat: copying files related to reset password feature because are very similar.

---
+++ EsmorgaiOS/App/Screens/ChangePassword/ChangePasswordView.swift
@@ -0,0 +1,86 @@
+//
+//  ChangePasswordView.swift
+//  EsmorgaiOS
+//
+//  Created by Moran, Marcelo on 27/8/26.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct ChangePasswordView: View {
+
+    @StateObject var viewModel: ChangePasswordViewModel
+    @Environment(\.dismiss) private var dismiss
+    @FocusState private var focusedField: ChangePasswordModels.TextFieldType?
+    init(viewModel: ChangePasswordViewModel) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+    }
+
+    var body: some View {
+        BaseView(viewModel: viewModel) {
+            VStack(alignment: .leading, spacing: 12) {
+                createTitleView()
+                    .padding(.bottom, 16.5)
+                    .padding(.top, 50)
+                VStack(alignment: .leading, spacing: 8) {
+                    ForEach(viewModel.textFields.indices, id: \.self) { index in
+                        CustomTextField(text: $viewModel.textFields[index].text,
+                                        caption: $viewModel.textFields[index].errorMessage,
+                                        isProtected: viewModel.textFields[index].isProtected,
+                                        title: viewModel.textFields[index].title,
+                                        hint: viewModel.textFields[index].placeholder)
+                        .onFocusChange { isFocused in
+                            if !isFocused {
+                                viewModel.validateTextField(type: viewModel.textFields[index].type, checkIsEmpty: true)
+                            }
+                        }
+                        .focused($focusedField, equals: viewModel.textFields[index].type)
+                    }
+                }
+                .padding(.bottom, 32)
+                LazyVStack(spacing: 16) {
+                    CustomButton(title: $viewModel.primaryButton.title,
+                                 buttonStyle: .primary,
+                                 isLoading: $viewModel.primaryButton.isLoading,
+                                 isDisabled: .constant(!viewModel.isFormValid)) {
+                        viewModel.performResetPassword()
+                    }
+                }
+                Spacer()
+            }
+            .padding(.init(top: 20, leading: 16, bottom: 16, trailing: 16))
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .navigationBarBackButtonHidden(true)
+    }
+}
+
+private func createTitleView() -> some View {
+    Text(LocalizationKeys.ResetPassword.title.localize())
+        .style(.heading1)
+}
+
+extension ChangePasswordView {
+
+    private func focusPreviousField() {
+        focusedField = focusedField.map {
+            ChangePasswordModels.TextFieldType(rawValue: $0.rawValue - 1) ?? .confirmPass
+        }
+    }
+
+    private func canFocusPreviousField() -> Bool {
+        guard let currentFocusedField = focusedField else {
+            return false
+        }
+        return currentFocusedField.rawValue > 0
+    }
+
+    private func canFocusNextField() -> Bool {
+        guard let currentFocusedField = focusedField else {
+            return false
+        }
+        return currentFocusedField.rawValue <  ChangePasswordModels.TextFieldType.allCases.count - 1
+    }
+}
---
+++ EsmorgaiOS/App/Screens/ChangePassword/ChangePasswordViewModel.swift
@@ -0,0 +1,152 @@
+//
+//  ChangePasswordViewModel.swift
+//  EsmorgaiOS
+//
+//  Created by Moran, Marcelo on 27/8/26.
+//
+
+import Foundation
+
+enum ChangePasswordViewStates: ViewStateProtocol {
+    case ready
+}
+
+class ChangePasswordViewModel: BaseViewModel<ActivateAccountViewStates> {
+
+    private let resetPasswordUserUseCase: ResetPasswordUserUseCaseAlias
+
+    @Published var textFields = [ChangePasswordModels.TextFieldModels]()
+
+    @Published var primaryButton = RecoverPasswordModels.Button(title: LocalizationKeys.Buttons.resetPassword.localize(), isLoading: false)
+
+
+
+    private let code: String
+
+    var isFormValid: Bool {
+        textFields.allSatisfy { tf in //true si cumple
+            !tf.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty && tf.errorMessage == nil
+        }
+        && (
+            textFields.first(where: { $0.type == .pass })?.text ==
+            textFields.first(where: { $0.type == .confirmPass })?.text
+        )
+    }
+
+    init(coordinator: (any CoordinatorProtocol)?,
+         resetPasswordUserUseCase: ResetPasswordUserUseCaseAlias = ResetPasswordUserUseCase(),
+         code: String = "") { // TODO: Delete 'code' parameter
+        self.resetPasswordUserUseCase = resetPasswordUserUseCase
+        self.code = code
+        super.init(coordinator: coordinator)
+
+        textFields =  [ChangePasswordModels.TextFieldModels(type: .pass,
+                                                            text: "",
+                                                            title: "Contraseña",
+                                                            placeholder: LocalizationKeys.TextField.Placeholders.password.localize(),
+                                                            isProtected: true),
+                       ChangePasswordModels.TextFieldModels(type: .pass,
+                                                           text: "",
+                                                           title: "Nueva Contraseña",
+                                                           placeholder: LocalizationKeys.TextField.Placeholders.newPassword.localize(),
+                                                           isProtected: true),
+                       ChangePasswordModels.TextFieldModels(type: .confirmPass,
+                                                           text: "",
+                                                           title: "Repetir Contraseña",
+                                                           placeholder: LocalizationKeys.TextField.Placeholders.confirmPassword.localize(),
+                                                           isProtected: true)]
+    }
+
+    private func validateAllFields() -> Bool {
+        var isValid: Bool = true
+        for textField in textFields {
+            if !validateTextField(type: textField.type, checkIsEmpty: true) {
+                isValid = false
+            }
+        }
+        return isValid
+    }
+
+    @discardableResult
+    func validateTextField(type: ChangePasswordModels.TextFieldType, checkIsEmpty: Bool) -> Bool {
+
+        guard let index = textFields.firstIndex(where: { $0.type == type }) else { return false }
+        textFields[index].text = textFields[index].text.trimmingCharacters(in: .whitespacesAndNewlines)
+
+        if getTextFieldIsValid(type: type, checkIsEmpty: checkIsEmpty) {
+            textFields[index].errorMessage = nil
+            return true
+        } else if textFields[index].text.isEmpty {
+            textFields[index].errorMessage = checkIsEmpty ? LocalizationKeys.TextField.InlineError.emptyField.localize() : nil
+            return !checkIsEmpty
+        } else {
+            textFields[index].errorMessage = getTextFieldErrorMessage(type: type)
+            return false
+        }
+    }
+
+    private func getTextFieldIsValid(type: ChangePasswordModels.TextFieldType, checkIsEmpty: Bool) -> Bool {
+
+        guard let index = textFields.firstIndex(where: { $0.type == type }) else { return false }
+        if type == .confirmPass {
+            if checkIsEmpty && textFields[index].text.isEmpty {
+                return false
+            } else {
+                guard let passTextIndex = textFields.firstIndex(where: { $0.type == .pass }) else { return false }
+                return textFields[passTextIndex].text == textFields[index].text
+            }
+        } else {
+            return textFields[index].text.isValid(regexPattern: getTextFieldRegex(type: type))
+        }
+    }
+
+    private func getTextFieldRegex(type: ChangePasswordModels.TextFieldType) -> RegexCase {
+        switch type {
+        case .oldPass, .pass, .confirmPass: return .userPassword
+        }
+    }
+
+    private func getTextFieldErrorMessage(type: ChangePasswordModels.TextFieldType) -> String {
+        switch type {
+        case .oldPass: return LocalizationKeys.TextField.InlineError.passwordInvalid.localize()
+        case .pass: return LocalizationKeys.TextField.InlineError.passwordInvalidLong.localize()
+        case .confirmPass: return LocalizationKeys.TextField.InlineError.passwordMismatch.localize()
+        }
+    }
+
+    @MainActor
+    func performResetPassword() {
+
+        guard validateAllFields() else { return }
+        primaryButton.isLoading = true
+
+        Task { [weak self] in
+            guard let self else { return }
+            let pass = textFields.first(where: { $0.type == .pass })?.text ?? ""
+
+            let result = await ResetPasswordUserUseCase().execute(input: ResetPasswordUserUseCaseInput(pass: pass, code: code))
+
+            await MainActor.run {
+                switch result {
+                case .success:
+                    self.primaryButton.isLoading = false
+                    UserDefaults.standard.set(true, forKey: "showSnackBarPassword")
+                    self.coordinator?.popToRoot()
+                    self.coordinator?.push(destination: .login)
+                case .failure(let error):
+                    self.primaryButton.isLoading = false
+                    switch error {
+                    default:
+                        self.showErrorDialog()
+                    }
+                }
+            }
+        }
+    }
+
+    private func showErrorDialog() {
+        let dialogModel = ErrorDialogModelBuilder.build(type: .commonError) {
+        }
+        coordinator?.push(destination: .dialog(dialogModel))
+    }
+}
